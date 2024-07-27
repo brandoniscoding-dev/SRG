371c4001b2a917e342cc4910c6ad9426f83e8d4d
brandoniscoding <brandoniscoding.dev@gmail.com>
2024-07-27 13:48:42 +0100

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SRG/",
+  base: '/SRG/',  // Assurez-vous que cette ligne correspond à votre dépôt GitHub Pages
 });
